8564e3fd10270927f922ea1d365a93dc9bac3d1d
ANU7MADHAV <anumadhavan888@gmail.com>
2023-12-10 17:19:52 +0530

    finished personal website

--- app/know-me/page.tsx
+++ app/know-me/page.tsx
@@ -1,7 +1,25 @@
-import React from "react";
-
 const FindMePage = () => {
-  return <div>FindMePage</div>;
+  return (
+    <div className="px-8 py-4 dark:bg-black dark:text-white">
+      <h1 className="pb-4 text-4xl font-medium">My coding journey</h1>
+      <p className="text-left dark:text-blue-500/60">
+        Starting out in coding, I once believed that JavaScript was a smaller
+        version of Java – a misconception that makes me chuckle now. Back then,
+        I didn&apos;t know the first thing about coding, but I decided to teach
+        myself. Self-learning was no walk in the park, but it instilled in me
+        the drive to learn and improve. I used to think I could be a perfect
+        coder, but I soon realized that technology moves too quickly to keep up
+        with everything. Embracing the idea that learning is a lifelong journey,
+        I&apos;ve discovered that consistency is crucial. Facing bugs and
+        issues, although initially overwhelming, became stepping stones to
+        progress. Now, my interest lies in Next.js, a frontend framework that
+        aligns perfectly with my passion for staying on top of the latest in
+        technology. My coding journey is a testament to the idea that growth
+        comes from challenges, adaptability, and a continuous pursuit of
+        knowledge.
+      </p>
+    </div>
+  );
 };
 
 export default FindMePage;
